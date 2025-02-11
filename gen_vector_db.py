981c0ef6b9b73ede238eb6caa86905b406ea9482
ivyyy0630 <weiliman000630@163.com>
2025-02-11 11:22:18 +0800

    Update gen_vector_db.py

--- gen_vector_db.py
+++ gen_vector_db.py
@@ -1,10 +1,11 @@
 import os
-source_folder = "results"
-from langchain.document_loaders import Docx2txtLoader
-from langchain.embeddings import OllamaEmbeddings
-from langchain.vectorstores import Chroma
+source_folder = "source/xiao/chengshen"
+collection_name = 'chengshen'
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain_experimental.text_splitter import SemanticChunker
-
+import time
 from common.read_config import EMBED_CONFIG
 
 # 配置 Embedding 和 LLM
@@ -24,7 +25,7 @@ for filename in os.listdir(source_folder):
 text_splitter = SemanticChunker(
     embeddings=ollama_embeddings,
     breakpoint_threshold_type="percentile",  # 可选：percentile, standard_deviation, interquartile
-    breakpoint_threshold_amount=95.0,  # 可根据需要调整
+    breakpoint_threshold_amount=85.0,  # 可根据需要调整
     number_of_chunks=None,  # 可选：指定分块数量
     sentence_split_regex=r"(?<=[。！？])\s*"  # 可根据需要调整句子分隔符
 )
@@ -32,13 +33,20 @@ chunked_documents = text_splitter.split_documents(documents)
 
 # 使用 Chroma 的持久化客户端保存嵌入结果
 persist_directory = "chroma_vector_db"  # 指定保存路径
-chroma_client = Chroma.from_documents(
-    documents=chunked_documents,
-    embedding=ollama_embeddings,
-    persist_directory=persist_directory  # 指定持久化路径
+chroma_client = Chroma(
+    embedding_function=ollama_embeddings,
+    persist_directory=persist_directory,
+    collection_name=collection_name,
 )
+for chunk in chunked_documents:
+    try:
+        chroma_client.add_documents([chunk])
+    except Exception as e:
+        print(f"Error processing chunk: {chunk}")
+    finally:
+        time.sleep(2)
 
 # 保存嵌入结果到本地
 chroma_client.persist()  # 持久化数据到指定路径
 
-print(f"嵌入结果已保存到本地目录：{persist_directory}")
+print(f"嵌入结果已保存到本地目录：{persist_directory}, {collection_name}")
